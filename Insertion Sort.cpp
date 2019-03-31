f19d43c298042ee051bd1e091296213620810004
qusehd422 <48934523+qusehd422@users.noreply.github.com>
2019-03-31 22:48:48 +0900

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -6,7 +6,7 @@ int main(void) {
 	int array[10] = { 1, 10, 5, 8, 7, 6, 4, 3, 2, 9 };
 	for (i = 0; i < 9; i++) {
 		j = i;
-		while (array[j] > array[j + 1]) {
+		while (j >= 0 && array[j] > array[j + 1]) {
 			temp = array[j];
 			array[j] = array[j + 1];
 			array[j + 1] = temp;
